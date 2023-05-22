127ec5b5bf1f6c58e7e190d0b6c1ab48e4b17e82
EdsonGomesJr <edyz86@hotmail.com>
2023-05-22 16:25:25 -0300

    4th class

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,22 @@
 import 'dotenv/config'
 import fastify from 'fastify'
-import {PrismaClient } from '@prisma/client'
+import multipart from '@fastify/multipart'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 
 const app = fastify()
 
+app.register(multipart)
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads'
+
+})
 app.register(cors, {
   origin: true,
 })
@@ -18,6 +26,7 @@ app.register(jwt, {
 })
 app.register(memoriesRoutes)
 app.register(authRoutes)
+app.register(uploadRoutes)
 
 app.listen({
   port: 3333,
